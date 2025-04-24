2edc41dc7d2ada4cb49167e9c179993ea41c5483
chuongln <46110900+chuongln@users.noreply.github.com>
2025-04-24 15:29:53 +0700

    init

--- lib/ma-lib.component.d.ts
+++
@@ -1,5 +0,0 @@
-import * as i0 from "@angular/core";
-export declare class MaLibComponent {
-    static ɵfac: i0.ɵɵFactoryDeclaration<MaLibComponent, never>;
-    static ɵcmp: i0.ɵɵComponentDeclaration<MaLibComponent, "lib-ma-lib", never, {}, {}, never, never, false, never>;
-}
